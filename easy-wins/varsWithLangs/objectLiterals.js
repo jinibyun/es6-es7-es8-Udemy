2b9260b4dba8887736518105a96a2122b181bee8
Jini Byun <jinibyun@gmail.com>
2021-05-17 12:36:51 -0400

    object literal and destructing object

--- easy-wins/varsWithLangs/objectLiterals.js
+++ easy-wins/varsWithLangs/objectLiterals.js
@@ -1,24 +1,42 @@
-// Old way:
+// ! 3 primary ways of making an object
 
+// 1. let x = new Object();
+// 2. let x = Object.create(null);
+// 3. let x = {}; // this is "object literal"
+
+
+
+
+
+// ! using object literal
+// Old way:
 let stuffFromAPI = {
 	name: "Rob",
 	career: "Teacher",
-	langauge: "JavaScript"
-}
+	langauge: "JavaScript",
+	aMethod : function(){
+		console.log("I teach");
+	}
+};
 
 let name = stuffFromAPI.name;
-let career = stuffFromAPI.career
-let langauge = stuffFromAPI.langauge
+let career = stuffFromAPI.career;
+let langauge = stuffFromAPI.langauge;
+const me = stuffFromAPI.aMethod();
 
+// New way:
 let newThingToPassToAPI = {
 	name: name,
 	career: career,
 	langauge: langauge
 }
 
-// new way
+// new way: better
 let coolNewWay = {
 	name,
 	career,
-	langauge
+	langauge,
+	aMethod(){
+		console.log("I teach");
+	}
 }
