3abab482733e51bb7635137eda2a1238446692b7
Jini Byun <jinibyun@gmail.com>
2021-05-19 14:26:48 -0400

    small utility: Array.of()

--- arrays/arrayOf.js
+++ arrays/arrayOf.js
@@ -1,16 +1,18 @@
-// Array.of()
-
-const arr1 = Array.of(7);
-const arr4 = [7];
+// ! Array.of()
+// 1   compare
+const arr1 = Array.of(7); // create 1 element  same as arr1= [7];
 console.log(arr1);
 
-const arr2 = Array(7);
-console.log(arr2)
+const arr2 = Array(7); // create 7 elements
+console.log(arr2);
 
-const arr3 = Array.of(7, "Hulk", [23,12,5],{sport:"baseball"})
+
+const arr3 = Array.of(7, "Hulk", [23,12,5],{sport:"baseball"});
 console.log(arr3);
 
-// Polyfill
+
+
+// !Polyfill: FYI, If the browser does not support
 if(!Array.of){
 	Array.of = function(){
 		return Array.prototype.slice.call(arguments);
