1e36e945b5d3f2143236997e502049154e6c944e
Jini Byun <jinibyun@gmail.com>
2021-05-19 14:54:29 -0400

    find and findIndex

--- arrays/findAndIndex.js
+++ arrays/findAndIndex.js
@@ -1,21 +1,26 @@
-const cityWeatherData = require ('./weatherData')
-// Array.prototype.find()
+// ! find and findIndex
+const cityWeatherData = require ('./weatherData'); // NOTE: weatherData should have "module.exports = cities;". Then we can import using "require"
 
-const cityNames = cityWeatherData.list.map((city)=>{
-	return city.name;
-})
+// 1. Classical map
+// const cityNames = cityWeatherData.list.map((city)=>{
+// 	return city.name;
+// })
 // console.log(cityNames);
 
-const firstFoundCity = cityWeatherData.list.find((city)=>{
-	if(city.main.temp > 80){
-		return true;
-	}else{
-		return false;
-	}
-})
-console.log(firstFoundCity);
 
-// Array.prototype.findIndex()
+
+// 2. Array.prototype.find() : return "first" element which makes condition as true
+// const firstFoundCity = cityWeatherData.list.find((city)=>{
+// 	if(city.main.temp > 80){
+// 		return true;
+// 	}else{
+// 		return false;
+// 	}
+// })
+// console.log(firstFoundCity);
+
+
+// 3. Array.prototype.findIndex(): return "element's index" which makes condition as true
 const firstFoundCityIndex = cityWeatherData.list.findIndex((city)=>{
 	if(city.main.temp > 80){
 		return true;
