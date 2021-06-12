0a3c3cf7723b8689790a1b77d3c57d23626859f4
Jini Byun <jinibyun@gmail.com>
2021-06-12 00:26:52 -0400

    async and await basic

--- promiseAsyncAwait/async.js
+++ promiseAsyncAwait/async.js
@@ -1,8 +1,10 @@
-// REQUIRES AT LEAST NODE 7.6 
+// ! REQUIRES AT LEAST NODE 7.6 
 // 2 new keywords:
 // 1. async
 // 2. await
 
+
+// ? BEFORE async and await
 function one(){
 	return new Promise((resolve, reject)=>{
 		setTimeout(()=>{
@@ -12,15 +14,18 @@ function one(){
 	});
 }
 
+// console.log("Calling function one")
+// one().then((promiseData)=>{
+	// console.log(promiseData)
+// });
+
+
+// ? 2 AFTER async and await: This is an exactly same thing as "then". but more convinient!!
 async function two(){
 	console.log("Calling function one")
 	const oneResponse = await one();
 	console.log(oneResponse)
 }
 
-// console.log("Calling function one")
-// one().then((promiseData)=>{
-	// console.log(promiseData)
-// });
 two();
 console.log("Last line of the code");
